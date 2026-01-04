bed2e8393e7732836e14bb96c28012bf3fe1fd94
Tinku112 <tinkukool.ts@gmail.com>
2026-01-04 06:15:24 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-mongoose.connect("mongodb://127.0.0.1:27017/personalityDB")
+mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB Connected"));
 
 app.use("/api/students", require("./routes/studentRoutes"));
