ac903a73ef731274be1fde9947e47a9937286997
Tinku112 <tinkukool.ts@gmail.com>
2026-01-04 06:27:58 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -13,9 +13,12 @@ mongoose.connect(process.env.MONGO_URI)
 
 app.use("/api/students", require("./routes/studentRoutes"));
 
-app.get("/", (req, res) => {
-  res.send("Server is live!");
+app.use(express.static(path.join(__dirname, "../client")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/index.html"));
 });
 
+// Use Render port or fallback to 5000
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
